705f86cc30321d30e2539cdcf241c87fff4ffe00
Aravind <aravind@pop-os.localdomain>
2021-03-15 20:29:12 +0530

    final touches

--- src/features/todoSlice.js
+++ src/features/todoSlice.js
@@ -11,8 +11,9 @@ const todoSlice = createSlice({
     saveTodo: (state, action) => {
       state.todoList.push(action.payload);
     },
-/* eslint-disable no-return-assign, no-param-reassign */
+
     saveCheck: (state, action) => {
+      /* eslint-disable no-return-assign, no-param-reassign */
       state.todoList.map(item => {
         if (action.payload === item.itemId) {
           if (item.done === true) {
@@ -24,10 +25,9 @@ const todoSlice = createSlice({
       }
       );
     },
-/* eslint-enable no-return-assign, no-param-reassign */
   },
 });
-
+/* eslint-disable no-return-assign, no-param-reassign */
 export const { saveTodo,saveCheck } = todoSlice.actions;
 export const selectTodoList = (state) => state.todos.todoList;
 export default todoSlice.reducer;
